e510dbee78c5aa8f9895f13e748617d735810926
Vaibhav Kumar <vaibhav.kumar@knoldus.com>
2022-07-20 08:44:28 +0530

    added login page

--- app.py
+++ app.py
@@ -3,8 +3,22 @@ from flask import Flask, request, render_template
 app = Flask(__name__, template_folder='./templates')
 
 @app.route('/', methods=['GET'])
-def index():
+def home():
     return render_template('index.html', title="Home Page")
+
+@app.route('/login', methods=['GET'])
+def login():
+    return render_template('login.html', title="Login Page")
+
+
+@app.route('/score', methods=['GET','POST'])
+def score():
+    if request.method == 'POST':
+        Name = request.form['Name']
+        Email = request.form['Email']
+        Batch = request.form['Batch']
+        Role = request.form['Role']
+        
 
 if __name__ == '__main__':
     app.debug = True
