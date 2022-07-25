76ca6f77bea1a6f1d3215aa60437ef6b5bd381bb
Vaibhav AWS <vaibhav.kumar@knoldus.com>
2022-07-25 16:42:12 +0000

    added changes fro password

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def register():
         name = form.name.data
         email = form.email.data
         username = form.username.data
-        password = sha256_crypt.encrypt(str(form.password.data))
+        password = sha256_crypt.hash(str(form.password.data))
 
         # Create cursor
         cur = mysql.connection.cursor()
